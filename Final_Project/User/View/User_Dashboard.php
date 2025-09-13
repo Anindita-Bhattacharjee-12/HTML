2f00767d7d8ad1b66162b0a83b7a0e7d57ff279b
Anindita-Bhattacharjee-12 <157709960+Anindita-Bhattacharjee-12@users.noreply.github.com>
2025-09-13 10:15:59 +0600

    Add new file

---
+++ Final_Project/User/View/User_Dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>User Dashboard</title>
+        <link rel="stylesheet" href="../CSS/User_Dashboard.css">
+    </head>
+    <body>
+    <div class="navbar">
+     <a href="AboutUs.php">About Us</a>   
+     <a href="SelfDefense.php">Safety Resources</a>  
+     <a href="User_View_Seminar.php">Seminar Schedule</a> 
+     <a href="Access_Emergency.php">Emergency Contact</a>
+     <a href="Safety_Notification.php">Safety Notification</a>
+     <a href="Home.php">Previous Page</a>
+    </div>
+    </body>
+</html>
---
+++ Final_Project/User/View/User_View_Seminar.php
@@ -0,0 +1,31 @@
+<?php
+include "../../Dashboard/config.php";
+$result=$conn->query("SELECT * FROM seminars");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>View Seminar</title>
+        <link rel="stylesheet" href="../CSS/User_View_Seminar.css">
+    </head>
+    <body>
+        <h1>Upcoming Program</h1>
+        <table border="2" cellpadding="10" class="form_design">
+            <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Date</th>
+            </tr>
+            <?php while($row=$result->fetch_assoc()){?>
+                <tr>
+                    <td><?php echo $row['Id'];?></td>
+                    <td><?php echo $row['Title'];?></td>
+                    <td><?php echo $row['Seminar_Date'];?></td>    
+                </tr>
+            <?php } ?>
+        </table>
+        <div style="text-align:center">
+        <a href="User_Dashboard.php" style="color:black;">Previous Page</a>
+        </div>
+    </body>
+</html>
